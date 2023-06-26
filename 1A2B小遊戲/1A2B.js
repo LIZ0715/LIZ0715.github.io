211b8849f0b68cd044252eb2e9c318edf0d2f1ca
LIZ <lin4353482@gmail.com>
2023-06-26 12:11:46 +0800

    add person json

--- 1A2B小遊戲/1A2B.js
+++ 1A2B小遊戲/1A2B.js
@@ -6,16 +6,17 @@ let resetButton = document.querySelector(".reset");
 let answerButton = document.querySelector(".answer");
 let historyRecord = document.querySelector(".record-board");
 
+//window載入
 window.onload = function () {
     alert("請按遊戲開始按鈕")
-    resetButton.disabled=true;
-    answerButton.disabled=true;
-    submitButton.disabled=true;
-    guessResult.length =0;
-    inputList.length=0;
+    resetButton.disabled = true;
+    answerButton.disabled = true;
+    submitButton.disabled = true;
+    guessResult.length = 0;
+    inputList.length = 0;
 }
 
-//產生4個不重複的亂數
+//產生4個不重複的亂數，當按下遊戲開始時產出4個不重複之亂數
 let ans = "";//宣告一個空字串拿來放ans
 function generateAnswer() {
     while (ans.length < 4) {
@@ -32,22 +33,22 @@ function initial() {
     answerButton.disabled = true;
     submitButton.disabled = true;
     historyRecord.innerHTML = "";
-    guessResult.length =0;
-    inputList.length=0;
+    guessResult.length = 0;
+    inputList.length = 0;
     alert(`記得按下遊戲開始按鈕歐`);
 
     startButton.disabled = false;
 }
+
 //start-game button
 startButton.addEventListener("click", startGame);
 function startGame() {
     generateAnswer();
     alert(`遊戲開始成功`)
     startButton.disabled = true;
-    
-    resetButton.disabled=false;
-    answerButton.disabled=false;
-    submitButton.disabled=false;
+    resetButton.disabled = false;
+    answerButton.disabled = false;
+    submitButton.disabled = false;
 }
 
 //submit button
@@ -59,10 +60,10 @@ function result() {
     let userInput = guessInput.value;
 
     //防呆機制
-    if (userInput.length === 4 && !isNaN(userInput)&& !hasDuplicate(userInput)) {
+    if (userInput.length === 4 && !isNaN(userInput) && !hasDuplicate(userInput)) {
         inputList.push(userInput);
     }
-    else{
+    else {
         alert(`請輸入正確數字`);
         guessInput.value = "";
         return;
@@ -78,43 +79,37 @@ function result() {
         }
     }
     guessResult.push(`${A}A${B}B`);
+    //判斷使用者輸入的4個數字是否有重複
+    function hasDuplicate(input) {
+        for (let k = 0; k < input.length; k++) {
+            for (let m = k + 1; m < input.length; m++) {
+                if (input[k] === input[m]) {
+                    return true;
+                }
+            }
+        }
+        return false;
 
-    
+    }
+
     //顯示在history-record上
     for (let i = guessResult.length - 1; i < guessResult.length; i++) {
         for (let j = inputList.length - 1; j < inputList.length; j++) {
-            const recordCount=j+1;
-            const currentTime = new Date().toLocaleTimeString();
-            historyRecord.innerHTML += "<br>"+"第"+recordCount +"次-"+ inputList[j] + " " + guessResult[i] +"----"+currentTime+ "<br>";
+            const recordCount = j + 1;//次數
+            const currentTime = new Date().toLocaleTimeString();//顯示時間
+            historyRecord.innerHTML += "<br>" + "第" + recordCount + "次-" + inputList[j] + " " + guessResult[i] + "----" + currentTime + "<br>";
         }
     }
-    
+
 
     if (A === 4) {
         alert(`猜對拉!`);
-        submitButton.disabled=true;
-        answerButton.disabled=true;
+        submitButton.disabled = true;
+        answerButton.disabled = true;
     }
 
     guessInput.value = "";
-
-
-
 }
-
-//判斷使用者輸入的4個數字是否有重複
-function hasDuplicate(input) {
-    for (let k = 0; k < input.length; k++) {
-        for(let m=k+1;m<input.length;m++){
-            if(input[k]===input[m]){
-                return true;
-            }
-        }
-    }
-    return false;
-
-}
-
 
 //看答案的button
 answerButton.addEventListener("click", lookAnswer);
